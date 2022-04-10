dbf5cf9da3d6d4214d9771aba6c7a91bb5682b99
JJH <naturalflavor1007@naver.com>
2022-04-10 10:22:17 +0900

    feat(setting): 빠진 컴포넌트 추가

--- openseafinder/src/pages/main/Main.jsx
+++ openseafinder/src/pages/main/Main.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import MainBody from "../../components/main/MainBody";
 
 function Main() {
-    return <div>메인페이지 입니다</div>;
+    return (
+        <>
+            <div>메인페이지 입니다</div>
+        </>
+    );
 }
 
 export default Main;
