9ca5cbd3934984e78230bafe6ad3fe6bc0703046
JH Park <54497150+juliajh@users.noreply.github.com>
2022-04-26 02:22:55 +0900

    header 연결

--- openseafinder/src/components/common/Header/Header.jsx
+++ openseafinder/src/components/common/Header/Header.jsx
@@ -1,15 +1,19 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import React from "react";
 import "./Header.css";
 
 function Header() {
+    function detailClick(e){
+        window.location.href="/detail";
+    }
     return (
         <div className="header">
             <div className="menuWrapper">
-                <div className="menuItem">
+                <div className="menuItem" onClick={detailClick}>
                     <span>Search Wallet</span>
                 </div>
-                <div className="menuItem">
-                    <span>Price Finder</span>
+                <div className="menuItem" onClick={detailClick}>
+                    <span>Price Finder </span>
                 </div>
                 <div className="menuItem">
                     <span>Docs</span>
