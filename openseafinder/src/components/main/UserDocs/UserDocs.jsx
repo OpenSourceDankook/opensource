20a902ed89031ffdbe898d5793fa2776206f2ff2
JaeHuiBoo <yeppyboo@naver.com>
2022-04-26 15:14:39 +0900

    userdev

--- openseafinder/src/components/main/UserDocs/UserDocs.jsx
+++ openseafinder/src/components/main/UserDocs/UserDocs.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./UserDocs.css";
 
 function UserDocs() {
+    const walletInfo = "WALLET INFO\n\n\n주 해킹 요소인 가상 지갑을 이용하는 대신 가상 지갑 주소를 사용";
+    const nftInfo = "NFT INFO\n\n\n블록기술을 이용해서 디지털 자산의 소유주를 증명하는 가상의 토큰 대상이 되는 그림, 영상 등의 디지털 파일을 가리키는 주소를 토큰안에 담음으로써 일종의 가상 진품 증명서"
     return (
         <div className="userDocs">
             <div className="userDocsHeader">
@@ -14,11 +16,15 @@ function UserDocs() {
             </div>
             <div className="userDocsBody">
                 <div className="cardItem">
-                    <span>WALLET INFO</span>
+                    <div>
+                        {walletInfo}
+                    </div>
                 </div>
                 <div className="logoWrapper"> LOGO </div>
                 <div className="cardItem">
-                    <span>NFT INFO</span>
+                    <div>
+                        {nftInfo}
+                    </div>
                 </div>
             </div>
         </div>
