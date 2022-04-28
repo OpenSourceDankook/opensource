485559780e99e6cbc6ec7ad3f8a83a9d01c633c5
JH Park <54497150+juliajh@users.noreply.github.com>
2022-04-29 01:49:36 +0900

    detail input

--- openseafinder/src/components/detail/DetailBody/DetailBody.jsx
+++ openseafinder/src/components/detail/DetailBody/DetailBody.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import "./DetailBody.css";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function DetailBody() {
   return (
     <div className="DetailBody">
       <div className="search">
-        <input type="text"></input>
+        <div class="form__group field">
+          <input type="input" class="form__field" placeholder="search" name="search" id='search' required />
+          <label for="search" class="form__label">Search</label>
+          <FontAwesomeIcon icon={faSearch} size="2x"/>
+        </div>
+        <div className="explain">
+          <div className="explainText">
+            <span>확인하려고 하는 프로젝트를 입력하세요.</span>
+          </div>
+        </div>
       </div>
+
     </div>
   );
 }
