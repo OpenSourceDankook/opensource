4fbd31d44ef5337dccd2594b0a0bf8ddafe6ff36
JaeHuiBoo <yeppyboo@naver.com>
2022-05-02 15:29:20 +0900

    detail_card_update

--- openseafinder/src/pages/detail/Detail_SW.jsx
+++ openseafinder/src/pages/detail/Detail_SW.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../../components/common/Header/Header";
 import DetailBody_SW from "../../components/detail/DetailBody/DetailBody_SW.jsx";
+import Card from "../../components/detail/Card.jsx";
 import { Link } from "react-router-dom";
 import "./Detail.css";
 
@@ -9,6 +10,7 @@ function Detail_SW() {
     <div className="detail">
       <Header />
       <DetailBody_SW />
+      <Card />
     </div>
   );
 }
