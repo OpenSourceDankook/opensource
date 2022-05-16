91d28f68feae279034c90fb33f8326c0b9afdf36
JH Park <54497150+juliajh@users.noreply.github.com>
2022-05-16 14:44:23 +0900

    반응형

--- openseafinder/src/components/common/Header/Header.jsx
+++ openseafinder/src/components/common/Header/Header.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import React from "react";
+import { useMediaQuery } from "react-responsive";
 import "./Header.css";
 
 function Header() {
@@ -13,21 +14,73 @@ function Header() {
     window.location.replace("/");
     document.body.scrollTop = document.body.scrollHeight;
   }
-  
-  return (
-    <div className="header">
-      <div className="menuWrapper">
-          <div className="menuItem" onClick={detailSWClick}>
-            <span>Search Wallet</span>
-          </div>
-          <div className="menuItem" onClick={detailClick}>
-            <span>Price Finder </span>
-          </div>
-          <div className="menuItem" onClick={docsClick}>
-            <span>Docs</span>
-          </div>
-      </div>
+
+  const Desktop = ({ children }) => {
+    const isDesktop = useMediaQuery({ minWidth: 992 });
+    return isDesktop ? children : null;
+  };
+  const Tablet = ({ children }) => {
+    const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 991 });
+    return isTablet ? children : null;
+  };
+  const Mobile = ({ children }) => {
+    const isMobile = useMediaQuery({ maxWidth: 767 });
+    return isMobile ? children : null;
+  };
+  const Default = ({ children }) => {
+    const isNotMobile = useMediaQuery({ minWidth: 768 });
+    return isNotMobile ? children : null;
+  };
+  /*
+  const Example = () => (
+    <div className="header" style="font-size:2rem; color:white;">
+      <Desktop>Desktop or laptop</Desktop>
+      <Tablet>Tablet</Tablet>
+      <Mobile>Mobile</Mobile>
+      <Default>Not mobile (desktop or laptop or tablet)</Default>
     </div>
+  );
+
+  */
+  return (
+    <>
+      <Desktop>
+        <div className="header">
+          <div className="menuWrapper">
+            <div className="menuItem" onClick={detailSWClick}>
+              <span>Search Wallet</span>
+            </div>
+            <div className="menuItem" onClick={detailClick}>
+              <span>Price Finder </span>
+            </div>
+            <div className="menuItem" onClick={docsClick}>
+              <span>Docs</span>
+            </div>
+          </div>
+        </div>
+      </Desktop>
+      <Tablet>
+        <div className="header">
+          <div className="menuWrapper">
+            <div className="menuItem" onClick={detailSWClick}>
+              <span>Search Wallet</span>
+            </div>
+          </div>
+        </div>
+      </Tablet>
+      <Mobile>
+        <div className="header">
+          <div className="menuWrapper">
+            <div className="menuItem" onClick={detailSWClick}>
+              <span>Search Wallet</span>
+            </div>
+            <div className="menuItem" onClick={detailClick}>
+              <span>Price Finder </span>
+            </div>
+          </div>
+        </div>
+      </Mobile>
+    </>
   );
 }
 
