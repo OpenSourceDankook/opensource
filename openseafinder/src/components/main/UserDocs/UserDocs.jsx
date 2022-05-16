8828cd80e97acb82b4e8c9448ad4f0a25eb669e8
JH Park <54497150+juliajh@users.noreply.github.com>
2022-05-16 15:06:26 +0900

    userDocs 글자 깨짐 해결

--- openseafinder/src/components/main/UserDocs/UserDocs.jsx
+++ openseafinder/src/components/main/UserDocs/UserDocs.jsx
@@ -2,39 +2,37 @@ import React from "react";
 import "./UserDocs.css";
 
 function UserDocs() {
-    const walletInfo = "주 해킹 요소인 가상 지갑을 이용하는 대신 가상 지갑 주소를 사용";
-    const nftInfo = "블록기술을 이용해서 디지털 자산의 소유주를 증명하는 가상의 토큰 대상이 되는 그림, 영상 등의 디지털 파일을 가리키는 주소를 토큰안에 담음으로써 일종의 가상 진품 증명서"
-    return (
-        <div className="userDocs">
-            <div className="userDocsHeader">
-                <div className="userDocsTitle">
-                    <span>USER DOCS</span>
-                </div>
-                <div className="userDocsDesc">
-                    <span>NFT finder 는 사용자 가이드를 제공하고 있습니다.</span>
-                </div>
-            </div>
-            <div className="userDocsBody">
-                <div className="cardItem">
-                    <div className="cardTitle">
-                        <h1>WALLET INFO</h1>
-                    </div>
-                    <div className="cardDetail">
-                        {walletInfo}
-                    </div>
-                </div>
-                <div className="logoWrapper"> LOGO </div>
-                <div className="cardItem">
-                    <div className="cardTitle">
-                        <h1>NFT INFO</h1>
-                    </div>
-                    <div className="cardDetail">
-                        {nftInfo}
-                    </div>
-                </div>
-            </div>
+  const walletInfo =
+    "주 해킹 요소인 가상 지갑을 이용하는 대신 가상 지갑 주소를 사용";
+  const nftInfo =
+    "블록기술을 이용해서 디지털 자산의 소유주를 증명하는 가상의 토큰 대상이 되는 그림, 영상 등의 디지털 파일을 가리키는 주소를 토큰안에 담음으로써 일종의 가상 진품 증명서";
+  return (
+    <div className="userDocs">
+      <div className="userDocsHeader">
+        <div className="userDocsTitle">
+          <span>USER DOCS</span>
         </div>
-    );
+        <div className="userDocsDesc">
+          <span>NFT finder 는 사용자 가이드를 제공하고 있습니다.</span>
+        </div>
+      </div>
+      <div className="userDocsBody">
+        <div className="cardItem">
+          <div className="cardTitle">
+            <h2>WALLET INFO</h2>
+          </div>
+          <div className="cardDetail">{walletInfo}</div>
+        </div>
+        <div className="logoWrapper"> LOGO </div>
+        <div className="cardItem">
+          <div className="cardTitle">
+            <h2>NFT INFO</h2>
+          </div>
+          <div className="cardDetail">{nftInfo}</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default UserDocs;
