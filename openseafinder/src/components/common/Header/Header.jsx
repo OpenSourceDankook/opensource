14ae8b91012a3581b76f24db4c1babf3ce0f1a19
kimyeonhong00 <63278864+kimyeonhong00@users.noreply.github.com>
2022-05-17 16:14:04 +0900

    반응형 헤더 하는중

--- openseafinder/src/components/common/Header/Header.jsx
+++ openseafinder/src/components/common/Header/Header.jsx
@@ -2,6 +2,10 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import React from "react";
 import "./Header.css";
 import { Desktop, Mobile } from "../MediaQuery.jsx";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faX } from "@fortawesome/free-solid-svg-icons";
+import {useState} from "react";
 
 function Header() {
   function detailClick(e) {
@@ -14,6 +18,15 @@ function Header() {
     window.location.replace("/");
     document.body.scrollTop = document.body.scrollHeight;
   }
+  
+  const [isOpen, setMenu] = useState(false);
+  const toggleMenu = () =>{
+    setMenu(isOpen => !isOpen);
+  }
+  function buttonClick(e){
+    window.location.href = "/detail"; //수정예정
+  }
+  
 
   return (
     <>
@@ -41,8 +54,17 @@ function Header() {
             <div className="menuItem" onClick={detailClick}>
               <span>Price Finder </span>
             </div>
+            <div className="menuItem" onClick={docsClick}>
+              <span>Docs</span>
+            </div>
+          </div>
+          <div className ="button" > 
+            <FontAwesomeIcon icon={ faBars }  />
+            {/* <div className={isOpen ? "show-menu" : "hide-menu"}></div> */}
+              
           </div>
         </div>
+        
       </Mobile>
     </>
   );
